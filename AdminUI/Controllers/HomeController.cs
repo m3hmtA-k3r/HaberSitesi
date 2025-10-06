f395b362c31980e74f48f6308e959cf6047bcae5
Mehmetasker <31123405+Aksaka7@users.noreply.github.com>
2025-10-06 13:53:34 +0200

    kısaltmala yapıldı

--- AdminUI/Controllers/HomeController.cs
+++ AdminUI/Controllers/HomeController.cs
@@ -5,18 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace AdminUI.Controllers
 {
 	[Authorize]
-	public class HomeController : Controller
+	public class HomeController(IHaberApiRequest haberApiRequest, IYorumApiRequest yorumApiRequest) : Controller
 	{
-		private readonly IHaberApiRequest _haberApiRequest;
-		private readonly IYorumApiRequest _yorumApiRequest;
-        public HomeController(IHaberApiRequest haberApiRequest,IYorumApiRequest yorumApiRequest)
-        {
-			_haberApiRequest = haberApiRequest;
-			_yorumApiRequest = yorumApiRequest;
-        }
+		private readonly IHaberApiRequest _haberApiRequest = haberApiRequest;
+		private readonly IYorumApiRequest _yorumApiRequest = yorumApiRequest;
+
         public IActionResult Index()
 		{
-            var haberler = _haberApiRequest.GetAllHaber().Count();
+            var haberler = _haberApiRequest.GetAllHaber().Count;
             var bekleyenYorumSayisi = _yorumApiRequest.GetOnayBekleyenYorumSayisi();
             ViewBag.BekleyenYorum = bekleyenYorumSayisi;
             ViewBag.Haberler = haberler;
